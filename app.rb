09a1a24695418fb00784c0fe13633d61944604ab
Calum Gilchrist <calum@homelinen.org>
2014-03-07 23:51:57 +0000

    Tidy Up and Error Handling

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ require 'haml'
 require 'aws-sdk'
 require 'dotenv'
 
-require './db.rb'
-
 class SinatraBootstrap < Sinatra::Base
     set :template, :layout
     set :dotenv, Dotenv.load
@@ -15,11 +13,6 @@ class SinatraBootstrap < Sinatra::Base
                            :secret => dotenv['SESSION_SECRET']
 
     # TODO: Generate a list of available AMIs
-    set :ami, 'ami-54cd3b23' 
-
-    set :aws, { access_key: dotenv['AWS_ACCESS_KEY'] }
-
-    set :db, Database.new.connect()
 
     def setup_ec2(access_key, secret_key)
         ec2 = AWS::EC2.new(
@@ -48,25 +41,29 @@ class SinatraBootstrap < Sinatra::Base
         # TODO: Get key from entry
         ec2 = setup_ec2(access_key, secret_key)
 
+        redirect to ('/') unless ec2
+
         session['access_key'] = access_key
         session['secret_key'] = secret_key
 
-        redirect to ('/') unless ec2
-
+        # Generate a single instance
+        # TODO: Allow a dropdown for size and AMI
         instance = ec2.instances.create(
             image_id: 'ami-e4625fa1',
             count: 1,
             instance_type: 't1.micro'
         )
 
-        #instance = { id: 'i-777777', dns_name: 'http://example.com' }
-
+        # Setup some helper session variables (Reduces API calls)
         session[:instance] = instance.id
-        session[:instance_url] = instance.dns_name
+        session[:instance_url] = 'http://' + instance.dns_name
 
         redirect to ('/summary')
     end
 
+    # Display the summary of instances
+    # 
+    # TODO: Grab instances from a database or the account itself
     get '/summary' do
         if not session[:access_key]
             redirect to ('/')
@@ -74,13 +71,20 @@ class SinatraBootstrap < Sinatra::Base
 
         ec2 = setup_ec2(session[:access_key], session[:secret_key]) 
 
-        instance = ec2.instances[session[:instance]]
+        begin
+            instance = ec2.instances[session[:instance]]
 
-        instance_url = instance.dns_name
+            instance_url = instance.dns_name
+        rescue AWS::EC2::Errors::InvalidInstanceID::NotFound
+           # If the instance cannot be found, signout
+           instance=nil 
+           redirect to('/signout')
+        end
 
         haml :summary, locals: { instance_id: instance.id, instance_url: instance_url, instance_status: instance.status }
     end
 
+    # TODO Combine most of stop and start together, reduce duplication
     get '/stop' do
         if not session[:access_key]
             redirect to ('/')
@@ -107,6 +111,12 @@ class SinatraBootstrap < Sinatra::Base
         ec2.instances[instance_id].start
 
         redirect to ('/summary')
+    end
+
+    get '/signout' do
+        session.clear
+
+        redirect to('/')
     end
 
     get '/stylesheets/application.css' do
